ef93030913f97b7aa1755b69509618ee8b5e067d
HusniFadhilah <husnfadhilah62@gmail.com>
2020-11-19 09:38:02 +0700

    Menambahkan middleware checkrole

---
+++ app/Http/Middleware/CheckRoleMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        foreach ($roles as $role) {
+            // Check if user has the role This check will depend on how your roles are set up
+            if ($user->hasRole($role))
+                return $next($request);
+        }
+
+        if ($request->isMethod('get')) {
+            abort(401);
+        } else if ($request->isMethod('post')) {
+            return redirect('/');
+        }
+    }
+}
