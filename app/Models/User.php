8bbddf8447bb6786379a30c91f56ae7f3c273f76
HusniFadhilah <husnfadhilah62@gmail.com>
2020-11-26 07:13:18 +0700

    Membuat model report, room, role & schedule

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        return null !== $this->role()->where('name', $role)->first();
+    }
 }
