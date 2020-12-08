9fd11c689beab97f2e3f7b0cc77ff79d0867c84a
HusniFadhilah <husnfadhilah62@gmail.com>
2020-12-08 09:06:47 +0700

    Menambahkan library fungsi berserta service providernya

--- app/Libraries/Fungsi.php
+++ app/Libraries/Fungsi.php
@@ -3,6 +3,7 @@
 namespace App\Libraries;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Format response.
@@ -22,5 +23,10 @@ class Fungsi
     public static function slice_string_by_word($string)
     {
         return Str::after($string, 'http://localhost:8000/storage/');
+    }
+
+    public static function get_role_session()
+    {
+        return strtolower(Auth::user()->role->name);
     }
 }
