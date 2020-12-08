e792e7542547186f6993c43ec689b304d007abfc
HusniFadhilah <husnfadhilah62@gmail.com>
2020-12-08 10:01:38 +0700

    Membuat view dashboard supervisor dari data yang ada di tabel

--- app/Http/Controllers/Supervisor/SupervisorController.php
+++ app/Http/Controllers/Supervisor/SupervisorController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Supervisor;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Room;
+use App\Models\User;
+use App\Models\Report;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SupervisorController extends Controller
 {
     public function index(Request $request)
     {
-        return view('supervisor.index');
+        $sum_cs = User::where('role_id', 2)->count();
+        $sum_room = Room::all()->count();
+        $sum_status_sudah = Report::where('status', 1)->count();
+        $sum_status_belum = Report::where('status', 0)->count();
+        $reports_today = Report::whereDate('date_time', Carbon::today())->get();
+        $reports = Report::all();
+        return view('supervisor.index', compact('sum_cs', 'sum_room', 'sum_status_sudah', 'sum_status_belum', 'reports_today', 'reports'));
     }
 }
