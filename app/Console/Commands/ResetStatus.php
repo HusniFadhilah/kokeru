a2ef590d0857c1f6af75c4c9704ff7659d3e0297
HusniFadhilah <husnfadhilah62@gmail.com>
2020-12-09 23:23:54 +0700

    Modifikasi file kernel

---
+++ app/Console/Commands/ResetStatus.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\Supervisor\ScheduleController;
+
+class ResetStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "For reset status all cleaning service's schedule";
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $schedule = new ScheduleController();
+        $schedule->reset();
+        $this->info("Successfully reset status all cleaning service's schedule");
+    }
+}
